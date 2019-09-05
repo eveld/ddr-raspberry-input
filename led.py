e9c7dca885fa3f0da3ed2fb51b18b1055496b9ea
Erik Veld <eveld@hashicorp.com>
2019-09-05 21:30:08 +0200

    flip colors

--- led.py
+++ led.py
@@ -1,7 +1,6 @@
 import board
 import neopixel
 
-pixels = neopixel.NeoPixel(board.D18, 250)#, brightness=1.0, auto_write=False, pixel_order=neopixel.GRB)
+pixels = neopixel.NeoPixel(board.D18, 150)#, brightness=1.0, auto_write=False, pixel_order=neopixel.GRB)
 
-pixels.fill((255, 0, 0))
-# pixels.show()
+pixels.fill((0, 255, 0))
