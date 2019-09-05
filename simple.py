0b0f00df267d6638bb2a7ca1f9539691df0fead8
Erik Veld <eveld@hashicorp.com>
2019-09-05 18:11:26 +0200

    up

--- simple.py
+++ simple.py
@@ -3,6 +3,6 @@
 from gpiozero import Button
 from time import sleep
 
-button = Button(16, pull_up=False)
+button = Button(16, pull_up=True)
 button.wait_for_press()
 print("The button was pressed!")
